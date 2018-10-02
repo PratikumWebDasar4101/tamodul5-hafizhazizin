5c976c129df1701874ce3c0aec04a83b1fde9eb9
Hafizh 'Azizi Nasution <42725297+hafizhazizin@users.noreply.github.com>
2018-10-02 17:53:38 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,19 @@
-<?php
-
-
-
-
-?>
+<form action="proseslogin.php" method="POST">
+<table align="center">
+    <tr>
+        <h2 align="center">Selamat Datang</h2>
+    </tr>
+    <tr>
+        <td>ID</td>
+        <td><input type="text" name="id"></td>
+    </tr>
+    <tr>
+        <td>Password</td>
+        <td><input type="password" name="password"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><input type="submit" name="login" value="Login"></td>
+    </tr>
+</table>
+</form>
