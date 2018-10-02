69292aaf81dda1cc6e5301cef0fb3eb5cdbe697c
Hafizh 'Azizi Nasution <42725297+hafizhazizin@users.noreply.github.com>
2018-10-02 17:39:00 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,32 @@
 <?php
+    session_start();
 
+    if(isset($_POST['nim'])){
+        $nim =$_POST['nim'];
+        $nama =$_POST['nama'];
+        $jk =$_POST['jk'];
+        $fakultas =$_POST['fakultas'];
+        $prodi =$_POST['prodi'];
+        $email =$_POST['email'];
+        $hobi =$_POST['hobby'];
+//===========================================
+    $_SESSION['nim'] = $_POST['nim'];
+    $_SESSION['nama'] = $_POST['nama'];
+    $_SESSION['jk'] = $_POST['jk'];
+    $_SESSION['fakultas'] = $_POST['fakultas'];
+    $_SESSION['prodi'] = $_POST['prodi'];
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['hobby'] = $_POST['hobby'];
+//============================================
+    $foto = $_FILES['file']['name'];
+    $tmp_foto = $_FILES['file']['tmp_name'];
+    $dir = 'foto/';
+    $target = $dir.$foto;
 
-
-
-
+        $_SESSION['file'] = $target;
+    if(!move_uploaded_file($tmp_foto, $target)){
+    die("gagal upload");
+    }
+    header("location:view.php");
+}
 ?>
